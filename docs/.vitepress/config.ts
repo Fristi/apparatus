106d72c11f658de306625ef6a88b908b073b4b13
Fristi <mark@vectos.net>
2026-05-07 10:43:55 +0200

    Fix docs

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
       { text: 'Guide', link: '/getting-started' },
       { text: 'GitHub', link: 'https://github.com/Fristi/apparatus' },
     ],
-    logo: '/logo.jpg',
     sidebar: [
       {
         text: 'Introduction',
